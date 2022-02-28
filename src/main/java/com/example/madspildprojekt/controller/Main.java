bf4f02027dab60c7059b8c70e7582b543a72a3ce
Mojens <moha135h@stud.kea.dk>
2022-02-28 14:52:32 +0100

    First commit

---
+++ src/main/java/com/example/madspildprojekt/controller/Main.java
@@ -0,0 +1,13 @@
+package com.example.madspildprojekt.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @Author Mohammad Adel Murtada
+ */
+@Controller
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
